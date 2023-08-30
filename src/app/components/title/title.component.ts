a4b3aac9bb2da752782d55c354de398f0a4658b1
Wellington félix <wellingt_felix@hotmail.com>
2023-08-30 20:38:29 -0300

    finalizando a estilização

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.css'],
+  styleUrls: ['./title.component.css', './title.component.responsive.css'],
 })
 export class TitleComponent {
   @Input()
